c09ac102182b38bbbcfc1b69f8626a6ff5515126
ren <naziz5602@gmail.com>
2023-10-26 12:36:12 +0700

    pb 1

---
+++ ArrayBilangan19v2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayBilangan19v2 {
+    public static void main(String[] args) {
+        int[] bil = new int [4];
+        bil[0] = (int)5.0;
+        bil[1] = 12867;
+        bil[2] = (int)7.5;
+        bil[3] = 2000000;
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+    }
+}
